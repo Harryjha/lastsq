f6a77aac88ce60d1e0d253a5c5934ff4f16d7193
Jatesh jha <161496166+Harryjha@users.noreply.github.com>
2025-01-07 15:59:27 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,13 +1,13 @@
 const mysql = require("mysql2/promise"); // Use promise-based mysql2
  
 const mysqlPool = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "12345678",
-    database: "student_portal",
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
+    host: "",
+    user: "",
+    password: "",
+    database: "",
+    waitForConnections: ,
+    connectionLimit: ,
+    queueLimit: ,
 });
 
 module.exports = mysqlPool;
